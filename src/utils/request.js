ae5a0e46ce93124c15803299c4e72b8bb292fdf3
root <root@workzio.svr>
2025-06-28 08:05:36 +0000

    first commit

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 const baseURLs = {
-  development: 'http://192.168.29.106:8083/', // API server url (Development)
-  production: 'https://api.ideauxbill.in/', // API server url (Production)
+  development: 'http://168.231.102.58:8060/', // API server url (Development)
+  production: 'https://apibilling.oslogroups.com/', // API server url (Production)
   // staging: 'https://dev.hibiller.com/',  // API server url (Staging)
 };
 
-const environment = process.env.NODE_ENV || 'development';
-// const environment = 'production';   
+//const environment = process.env.NODE_ENV || 'development';
+const environment = 'production';
 
 const request = axios.create({
   baseURL: baseURLs[environment],
