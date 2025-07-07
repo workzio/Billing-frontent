9456a5fe0d12ce5f7cb029fe09a79087340d6434
workzio <workziotech@gmail.com>
2025-07-07 11:35:14 +0530

    Update index.js

--- src/Modules/HeadOffice/Company/CompanyReport/index.js
+++ src/Modules/HeadOffice/Company/CompanyReport/index.js
@@ -6,11 +6,9 @@ import { TopTitle } from '../../../../Components/Form/TopTitle'
 export const CompanyReport = () => {
   return (
     <Fragment>
-<<<<<<< HEAD
-      <TopTitle Heading={'OutSource Company Report'} />
-=======
+
       <TopTitle Heading={'OutSource Distributor Report'} />
->>>>>>> c2084415200b4927070204d83a4aeb64c0a89595
+
 
       <TopSection />
     </Fragment>
